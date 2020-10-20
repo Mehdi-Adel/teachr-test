f30de2fdf6f8e37e0bda1437cb62bc7e5cbba95e
Mehdi-Adel <zeadel.mehdi@gmail.com>
2020-10-20 21:15:12 +0200

    tableau multi-dimensionnel pour les cours

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -12,8 +12,51 @@ class CoursesController extends AbstractController
      */
     public function index()
     {
+        $courses = [
+            [
+                'lesson' => "Mathématiques",
+                'next_course' => "Aujourd'hui",
+                'teacher_name' => "Mehdi",
+                'schedule' => "Lundi et Mardi",
+                'starting_day' => "samedi 3 octobre",
+                'duration' => "1h30 par jour",
+                'starting_hour' => "14h30",
+                'whatever' => "Jérémy",
+            ],
+            [
+                'lesson' => "Philosophie",
+                'next_course' => "lundi 5 octobre",
+                'teacher_name' => "Serge",
+                'schedule' => "Lundi",
+                'starting_day' => "lundi 5 octobre",
+                'duration' => "1h00 par jour",
+                'starting_hour' => "10h30",
+                'whatever' => "Sandy",
+            ],
+            [
+                'lesson' => "Physique",
+                'next_course' => "Aujourd'hui",
+                'teacher_name' => "Kevin",
+                'schedule' => "Mardi et Mercredi",
+                'starting_day' => "mardi 6 octobre",
+                'duration' => "1h30 par jour",
+                'starting_hour' => "8h30",
+                'whatever' => "Dexiong",
+            ],
+            [
+                'lesson' => "Biochimie",
+                'next_course' => "Aujourd'hui",
+                'teacher_name' => "Cornélia",
+                'schedule' => "Lundi et Vendredi",
+                'starting_day' => "samedi 3 octobre",
+                'duration' => "1h30 par jour",
+                'starting_hour' => "15h00",
+                'whatever' => "Amel",
+            ],
+        ];
+
         return $this->render('courses/index.html.twig', [
-            'controller_name' => 'CoursesController',
+            'courses' => $courses,
         ]);
     }
 }
